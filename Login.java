c7a82efdf3c1d78e5daff39bf7baa40571e706d9
adilahdiyana <81138230+adilahdiyana@users.noreply.github.com>
2021-06-30 21:59:44 +0800

    Update Login.java

--- Login.java
+++ Login.java
@@ -1,29 +1,36 @@
-package Assignment3;
+package Assignment_3;
 
-import java.awt.BorderLayout;
+
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import java.awt.Color;
-import java.awt.Component;
-
 import javax.swing.border.MatteBorder;
+
+import java.awt.Color;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 
 import java.awt.Font;
-import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JButton;
 import javax.swing.ImageIcon;
-import javax.swing.SwingConstants;
-import javax.swing.JDesktopPane;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
 
 public class Login extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField UserNameTextField;
+	private JTextField PasswordTextField;
+	private String fmf, tcf, ddf;
 
 	/**
 	 * Launch the application.
@@ -31,7 +38,6 @@ public class Login extends JFrame {
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
-				//2.2 Exception Handling
 				try {
 					Login frame = new Login();
 					frame.setVisible(true);
@@ -42,80 +48,326 @@ public class Login extends JFrame {
 		});
 	}
 
+	public String getFashionModernFee() {
+		return this.fmf;
+	}
+
+	public String getTraditionalCustomsFee() {
+		return this.tcf;
+	}
+
+	public String getDinnerDressFee() {
+		return this.ddf;
+	}
+
 	/**
 	 * Create the frame.
 	 */
 	public Login() {
+		setTitle("Clothing Rental Business");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 995, 688);
+		setBounds(0, 0, 998, 562); // set for 0,0,1400,800
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+
+		JMenu mnNewMenu = new JMenu("Home");
+		menuBar.add(mnNewMenu);
+
+		JMenuItem mntmNewMenuItem_7 = new JMenuItem("Home");
+		mntmNewMenuItem_7.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ClothingRental frame = new ClothingRental();
+				frame.setModalExclusionType(null);
+				frame.setVisible(true);
+				dispose();
+			}
+		});
+		mnNewMenu.add(mntmNewMenuItem_7);
+
 		JPanel panel = new JPanel();
-		panel.setBorder(new MatteBorder(5, 5, 5, 5, (Color) new Color(210, 105, 30)));
 		panel.setBackground(new Color(250, 240, 230));
-		panel.setBounds(0, 0, 977, 641);
+		panel.setBorder(new MatteBorder(6, 6, 6, 6, (Color) new Color(128, 0, 0)));
+		panel.setBounds(0, 0, 985, 505);
 		contentPane.add(panel);
 		panel.setLayout(null);
-		
-		JButton btnExit = new JButton("Exit");
-		btnExit.addActionListener(new ActionListener() {
-			private Component frame;
 
+		UserNameTextField = new JTextField();
+		UserNameTextField.setBackground(new Color(230, 230, 250));
+		UserNameTextField.setBounds(616, 116, 265, 53);
+		UserNameTextField.setFont(new Font("Tahoma", Font.BOLD, 20));
+		panel.add(UserNameTextField);
+		UserNameTextField.setColumns(10);
+
+		PasswordTextField = new JTextField();
+		PasswordTextField.setBackground(new Color(230, 230, 250));
+		PasswordTextField.setBounds(616, 189, 265, 53);
+		PasswordTextField.setFont(new Font("Tahoma", Font.BOLD, 20));
+
+		PasswordTextField.setColumns(10);
+		panel.add(PasswordTextField);
+
+		JComboBox UserComboBox = new JComboBox();
+		UserComboBox.setBackground(new Color(255, 192, 203));
+		UserComboBox.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+		UserComboBox.setModel(new DefaultComboBoxModel(new String[] {"Select", "Customer", "Admin"}));
+		UserComboBox.setBounds(616, 285, 265, 46);
+		panel.add(UserComboBox);
+
+		JButton LoginButton = new JButton("LOGIN");
+		LoginButton.setBackground(new Color(250, 235, 215));
+		LoginButton.setBounds(646, 364, 115, 53);
+		LoginButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				frame = new JFrame();
-				if (JOptionPane.showConfirmDialog(frame, "Confirm if you want to exit", "LOGIN", 
-						JOptionPane.YES_NO_OPTION) == JOptionPane.YES_NO_OPTION) {
-					System.exit(0);
+				try { // 2.2 Exception Handling
+					if (UserNameTextField.getText().equals("adilah") && PasswordTextField.getText().equals("ad")
+							&& UserComboBox.getSelectedItem().equals("Admin")) {
+						JOptionPane.showMessageDialog(null, "Login Succesful!");
+						AdminCompany a = new AdminCompany(); // calling another frame/window
+						a.setModalExclusionType(null);
+						a.setVisible(true);
+						dispose();
+
+					}
+					for (int i = 0; i < UserRegistration.table.getRowCount(); i++) {
+
+						if (UserNameTextField.getText().equals(UserRegistration.table.getValueAt(i, 0))
+								&& PasswordTextField.getText().equals(UserRegistration.table.getValueAt(i, 1))
+								&& UserComboBox.getSelectedItem().equals("Customer")) {
+							JOptionPane.showMessageDialog(null, "Login Succesful!");
+							CustomerRegistration gm = new CustomerRegistration(); // calling another frame/window
+							gm.setModalExclusionType(null);
+							gm.setVisible(true);
+							dispose();
+						}
+					}
+					if (UserComboBox.getSelectedItem().equals("Select User")) {
+						JOptionPane.showMessageDialog(null, "Please select the correct user!");
+					}
+				} catch (Exception ex) {
+					JOptionPane.showMessageDialog(null, "Please enter the correct user and password!");
 				}
+
 			}
 		});
-		btnExit.setFont(new Font("STZhongsong", Font.PLAIN, 20));
-		btnExit.setBackground(new Color(250, 235, 215));
-		btnExit.setBounds(381, 489, 145, 76);
-		panel.add(btnExit);
-		
-		JLabel lblNewLabel = new JLabel("Hi, welcome! Kindly choose the type of user :)");
-		lblNewLabel.setBackground(Color.BLACK);
-		lblNewLabel.setForeground(new Color(165, 42, 42));
-		lblNewLabel.setFont(new Font("Rockwell", Font.BOLD, 30));
-		lblNewLabel.setBounds(124, 82, 787, 76);
-		panel.add(lblNewLabel);
-		
-		JButton btnAdminButton = new JButton("Admin");
-		btnAdminButton.setBackground(new Color(250, 235, 215));
-		btnAdminButton.addActionListener(new ActionListener() {
+		LoginButton.setFont(new Font("STXinwei", Font.BOLD, 18));
+		panel.add(LoginButton);
+
+		JButton ResetButton = new JButton("RESET");
+		ResetButton.setBackground(new Color(250, 235, 215));
+		ResetButton.setBounds(727, 430, 115, 53);
+		ResetButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				AdminLogin AL = new AdminLogin();
-				AL.setModalExclusionType(null);
-				AL.setVisible(true);
-				dispose();
+				UserNameTextField.setText("");
+				PasswordTextField.setText("");
 			}
 		});
-		btnAdminButton.setFont(new Font("STZhongsong", Font.PLAIN, 20));
-		btnAdminButton.setBounds(381, 226, 145, 76);
-		panel.add(btnAdminButton);
-		
-		JButton btnCustomer = new JButton("Customer");
-		btnCustomer.setBackground(new Color(250, 235, 215));
-		btnCustomer.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				CustLogin CL = new CustLogin();
-				CL.setModalExclusionType(null);
-				CL.setVisible(true);
-				dispose();
-			}
-		});
-		btnCustomer.setFont(new Font("STZhongsong", Font.PLAIN, 20));
-		btnCustomer.setBounds(381, 362, 145, 76);
-		panel.add(btnCustomer);
-		
+		ResetButton.setFont(new Font("STXinwei", Font.BOLD, 18));
+		panel.add(ResetButton);
+
 		JLabel lblNewLabel_1 = new JLabel("");
-		lblNewLabel_1.setForeground(new Color(210, 105, 30));
-		lblNewLabel_1.setBackground(new Color(250, 240, 230));
-		lblNewLabel_1.setBounds(0, 0, 977, 641);
+		lblNewLabel_1.setBounds(28, 97, 489, 307);
 		panel.add(lblNewLabel_1);
+		lblNewLabel_1.setIcon(new ImageIcon("C:\\Users\\Lenovo\\Downloads\\rentpic.png"));
+
+		JLabel TitleLabel = new JLabel("Clothing Rental Business");
+		TitleLabel.setForeground(new Color(255, 255, 255));
+		TitleLabel.setFont(new Font("Script MT Bold", Font.BOLD, 40));
+		TitleLabel.setBounds(259, 10, 696, 76);
+		panel.add(TitleLabel);
+
+		JButton registerButton = new JButton("REGISTER");
+		registerButton.setBackground(new Color(250, 235, 215));
+		registerButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				UserRegistration gm = new UserRegistration(); // calling another frame/window
+				gm.setModalExclusionType(null);
+				gm.setVisible(true);
+				dispose();
+			}
+		});
+		registerButton.setFont(new Font("STXinwei", Font.BOLD, 18));
+		registerButton.setBounds(783, 364, 135, 53);
+		panel.add(registerButton);
+
+		JLabel lblUserName = new JLabel("Username");
+		lblUserName.setBackground(new Color(0, 0, 0));
+		lblUserName.setForeground(new Color(0, 0, 0));
+		lblUserName.setFont(new Font("Times New Roman", Font.BOLD, 20));
+		lblUserName.setBounds(527, 129, 125, 31);
+		panel.add(lblUserName);
+
+		JLabel lblPassword = new JLabel("Password");
+		lblPassword.setForeground(new Color(0, 0, 0));
+		lblPassword.setFont(new Font("Times New Roman", Font.BOLD, 20));
+		lblPassword.setBounds(527, 202, 115, 31);
+		panel.add(lblPassword);
+
+		JLabel lblUser = new JLabel("User");
+		lblUser.setForeground(new Color(0, 0, 0));
+		lblUser.setFont(new Font("Times New Roman", Font.BOLD, 20));
+		lblUser.setBounds(527, 293, 115, 31);
+		panel.add(lblUser);
+
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\Lenovo\\Downloads\\clothpic.jpeg"));
+		lblNewLabel.setBounds(10, 10, 965, 485);
+		panel.add(lblNewLabel);
 	}
-}
+
+	public Login(String fmf, String tcf, String ddf) {
+		this.fmf = fmf;
+		this.tcf = tcf;
+		this.ddf= ddf;
+
+		setTitle("Clothing Rental Business");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(0, 0, 998, 562); // set for 0,0,1400,800
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+
+		JMenu mnNewMenu = new JMenu("Home");
+		menuBar.add(mnNewMenu);
+
+		JMenuItem mntmNewMenuItem_7 = new JMenuItem("Home");
+		mntmNewMenuItem_7.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ClothingRental frame = new ClothingRental();
+				frame.setModalExclusionType(null);
+				frame.setVisible(true);
+				dispose();
+			}
+		});
+		mnNewMenu.add(mntmNewMenuItem_7);
+
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(255, 255, 204));
+		panel.setBorder(new MatteBorder(6, 6, 6, 6, (Color) new Color(244, 164, 96)));
+		panel.setBounds(0, 0, 985, 505);
+		contentPane.add(panel);
+		panel.setLayout(null);
+
+		UserNameTextField = new JTextField();
+		UserNameTextField.setBounds(690, 107, 265, 53);
+		UserNameTextField.setFont(new Font("Tahoma", Font.BOLD, 20));
+		panel.add(UserNameTextField);
+		UserNameTextField.setColumns(10);
+
+		PasswordTextField = new JTextField();
+		PasswordTextField.setBounds(690, 189, 265, 53);
+		PasswordTextField.setFont(new Font("Tahoma", Font.BOLD, 20));
+
+		PasswordTextField.setColumns(10);
+		panel.add(PasswordTextField);
+
+		JComboBox UserComboBox = new JComboBox();
+		UserComboBox.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+		UserComboBox.setModel(new DefaultComboBoxModel(new String[] { "Select User", "Participant", "Admin" }));
+		UserComboBox.setBounds(690, 285, 265, 46);
+		panel.add(UserComboBox);
+
+		JButton LoginButton = new JButton("LOGIN");
+		LoginButton.setBounds(676, 364, 115, 53);
+		LoginButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {// 2.2 Exception Handling
+					if (UserNameTextField.getText().equals("adilah") && PasswordTextField.getText().equals("ad")
+							&& UserComboBox.getSelectedItem().equals("Admin")) {
+						JOptionPane.showMessageDialog(null, "Login Succesful!");
+						AdminCompany a = new AdminCompany(); // calling another frame/window
+						a.setModalExclusionType(null);
+						a.setVisible(true);
+						dispose();
+
+					}
+
+					if (UserNameTextField.getText().equals("alex") && PasswordTextField.getText().equals("12345")
+							&& UserComboBox.getSelectedItem().equals("Customer")) {
+						JOptionPane.showMessageDialog(null, "Login Succesful!");
+						CustomerRegistration gm = new CustomerRegistration(getFashionModernFee(), getTraditionalCustomsFee(),
+								getDinnerDressFee()); // calling another frame/window
+						gm.setModalExclusionType(null);
+						gm.setVisible(true);
+						dispose();
+					}
+
+					if (UserComboBox.getSelectedItem().equals("Select User")) {
+						JOptionPane.showMessageDialog(null, "Please select the correct user!");
+					}
+				} catch (Exception ex) {
+					JOptionPane.showMessageDialog(null, "Please enter the correct user and password!");
+				}
+
+			}
+		});
+		LoginButton.setFont(new Font("Times New Roman", Font.BOLD, 18));
+		panel.add(LoginButton);
+
+		JButton ResetButton = new JButton("RESET");
+		ResetButton.setBounds(529, 364, 115, 53);
+		ResetButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				UserNameTextField.setText("");
+				PasswordTextField.setText("");
+			}
+		});
+		ResetButton.setFont(new Font("Times New Roman", Font.BOLD, 18));
+		panel.add(ResetButton);
+
+		JLabel lblNewLabel_1 = new JLabel("");
+		lblNewLabel_1.setBounds(0, 46, 533, 475);
+		panel.add(lblNewLabel_1);
+		lblNewLabel_1.setIcon(new ImageIcon("C:\\Users\\Lenovo\\Downloads\\rentpic.png"));
+
+		JLabel TitleLabel = new JLabel("Ultimate Gamer Center System");
+		TitleLabel.setForeground(Color.WHITE);
+		TitleLabel.setFont(new Font("Tempus Sans ITC", Font.BOLD, 40));
+		TitleLabel.setBounds(200, 21, 696, 76);
+		panel.add(TitleLabel);
+
+		JButton registerButton = new JButton("REGISTER");
+		registerButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				UserRegistration gm = new UserRegistration(); // calling another frame/window
+				gm.setModalExclusionType(null);
+				gm.setVisible(true);
+				dispose();
+			}
+		});
+		registerButton.setFont(new Font("Times New Roman", Font.BOLD, 18));
+		registerButton.setBounds(821, 364, 135, 53);
+		panel.add(registerButton);
+
+		JLabel lblUserName = new JLabel("USERNAME");
+		lblUserName.setForeground(Color.WHITE);
+		lblUserName.setFont(new Font("Times New Roman", Font.BOLD, 20));
+		lblUserName.setBounds(487, 126, 125, 31);
+		panel.add(lblUserName);
+
+		JLabel lblPassword = new JLabel("PASSWORD");
+		lblPassword.setForeground(Color.WHITE);
+		lblPassword.setFont(new Font("Times New Roman", Font.BOLD, 20));
+		lblPassword.setBounds(487, 211, 115, 31);
+		panel.add(lblPassword);
+
+		JLabel lblUser_1 = new JLabel("USER");
+		lblUser_1.setForeground(Color.WHITE);
+		lblUser_1.setFont(new Font("Times New Roman", Font.BOLD, 20));
+		lblUser_1.setBounds(496, 296, 115, 31);
+		panel.add(lblUser_1);
+
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\Lenovo\\Downloads\\rentpic.png"));
+		lblNewLabel.setBounds(10, 10, 965, 485);
+		panel.add(lblNewLabel);
+	}
+} //end class of Login
